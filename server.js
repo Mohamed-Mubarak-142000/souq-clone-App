e6d729feeb6a2dae2e5a7cf154b068d6aeb853d9
Mohamed-Mubarak-142000 <156623820+Mohamed-Mubarak-142000@users.noreply.github.com>
2024-01-15 02:30:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ import cors from "cors";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
 
 dotenv.config();
 connectedDB();
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json());
